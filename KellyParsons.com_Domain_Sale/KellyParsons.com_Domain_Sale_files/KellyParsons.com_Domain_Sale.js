de08e03965e3de3f24da2556059e548275dce605
Tom Mitchell <tommitchell@me.com>
2017-05-17 18:48:44 -0400

    Domain Sale Notice Website

---
+++ KellyParsons.com_Domain_Sale/KellyParsons.com_Domain_Sale_files/KellyParsons.com_Domain_Sale.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170516
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('KellyParsons.com_Domain_Sale_files/KellyParsons.com_Domain_SaleMoz.css')
+adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');applyEffects()}
